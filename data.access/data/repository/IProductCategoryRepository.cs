141a4824bbc3e6ae6c286e816b6a38e951c2ecbe
manuXD270516 <manueljss@live.com>
2022-11-21 22:20:35 -0400

    feat(DEV): initial comit

---
+++ data.access/data/repository/IProductCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using activate_assurance.Models;
+using data.access.data.repository;
+using models;
+using models.erp.module;
+
+namespace data.access
+{
+	public interface IProductCategoryRepository : IRepository<ProductCategory>
+	{
+
+
+	}
+}
+
---
+++ data.access/data/repository/impl/ProductCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using activate_assurance.Models;
+using data.access.data.repository.impl;
+using Microsoft.EntityFrameworkCore;
+using models.erp.module;
+
+namespace data.access
+{
+	public class ProductErpRepository : Repository<ProductErp>, IProductErpRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductErpRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
+
